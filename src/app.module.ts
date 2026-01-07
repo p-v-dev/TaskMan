b1b578a92f1ec230c22a684a14baa56b035c1db8
pv.dev <pedro.v.r.brito@gmail.com>
2026-01-07 19:45:26 -0300

    .env protect

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna o módulo disponível em toda a aplicação
+      envFilePath: '.env', // Caminho opcional (padrão é a raiz)
+    }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,15 +5,17 @@ import { Repository } from "typeorm"
 import { User } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { JwtPayload } from "./jwt-payload.interface";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'topsecret',
+      secretOrKey: configService.get<string>('SECRET') || 'fallback_secret_caso_vazio',
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     })
   }
