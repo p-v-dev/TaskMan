8123758bad52d07fd022e807b4bcdecce840785f
p-v-dev <pedro.v.r.brito@gmail.com>
2026-03-16 23:58:25 -0300

    fix app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,26 +2,28 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // Torna o módulo disponível em toda a aplicação
-      envFilePath: '.env', // Caminho opcional (padrão é a raiz)
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: { rejectUnauthorized: false },
+        retryAttempts: 10,
+        retryDelay: 3000,
+      }),
     }),
     TasksModule,
-    TypeOrmModule.forRoot({
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  autoLoadEntities: true,
-  synchronize: true,
-  ssl: { rejectUnauthorized: false },
-  retryAttempts: 10,       // tenta 10 vezes
-  retryDelay: 3000,        // espera 3s entre tentativas
-}),
     AuthModule,
   ],
 })
-export class AppModule { }
+export class AppModule {}
